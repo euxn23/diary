09c448ae3226141a50b61bba6c0d9caaa6ee1ddc
euxn23 <euxn23@gmail.com>
2024-02-27 00:49:23 +0900

    chore: add config

---
+++ config.ts
@@ -0,0 +1,8 @@
+export const config = {
+  SITE_NAME: "diary.euxn.me",
+  OGP_WORKER: "https://ogp.worker.diary.euxn.me",
+  OG_IMAGE_URL: "https://ogp.diary.euxn.me/cache/og-image.png",
+  DESCRIPTION: "A diary by @euxn23",
+  KEYWORDS: "euxn23,diary,euxn.me,ユーン",
+  THEME: "light",
+};
