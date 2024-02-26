8862b6ec87b8f50cadf5c2de57aa8e247fec791a
euxn23 <euxn23@gmail.com>
2024-02-27 01:58:13 +0900

    chore: add other works

--- other-works.ts
+++ other-works.ts
@@ -1,3 +1,18 @@
 export async function getOtherWorks() {
-  return [];
+  const {
+    data: { contents: noteContents },
+  } = await fetch(
+    "https://note.com/api/v2/creators/euxn/contents?kind=note&page=1"
+  ).then((res) => res.json());
+  const noteEntries = noteContents.map((content: any) => {
+    const date = new Date(content.publishAt);
+    return {
+      title: content.name,
+      url: content.noteUrl,
+      media: "note",
+      date: `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`,
+    };
+  });
+
+  return noteEntries;
 }
