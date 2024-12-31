d70b73868f26976aa80e4233c84282f612516a36
euxn23 <euxn23@gmail.com>
2024-12-31 23:57:47 +0900

    feat: add entry

--- config.ts
+++ config.ts
@@ -1,6 +1,7 @@
 export const config = {
   SITE_NAME: "diary.euxn.me",
   SITE_URL: "https://diary.euxn.me/",
+  STATIC_FILE_URL: "https://static.diary.euxn.me",
   OGP_WORKER: "https://ogp.worker.diary.euxn.me",
   OG_IMAGE_URL: "https://ogp.diary.euxn.me/cache/og-image.png",
   DESCRIPTION: "A diary by @euxn23",
